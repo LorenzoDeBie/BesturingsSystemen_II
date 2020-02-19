90a6ef5a8aab79beba5ca5d29231c134900faf5d
LorenzoDeBie <lorenzodebie@ndbsoft.be>
2020-02-19 21:10:06 +0100

    Labo2: ex 1-6

--- Labo2/5.c
+++ Labo2/5.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <string.h>
 
 void standaard() {
 	char buffer[BUFSIZ];
@@ -30,6 +31,13 @@ int main(int argc, char **argv) {
 			//hier eerst nog checken of bestand wel uitschrijfbaar is
 			//open file
 			int fd = open(argv[i], O_RDONLY);
+			struct stat st;
+			fstat(fd, &st);
+			if((st.st_mode & S_IFMT) != S_IFREG) {		
+				write(0, argv[i], strlen(argv[i]));	
+				write(0," is not a file!\n", strlen(" is not a file!\n"));
+				continue;
+			}
 			if(fd < 0) {
 				perror(argv[i]);
 				continue;
---
+++ Labo2/6.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	//cp src dest
+	//give error when src or dest is not a file
+	if(argc != 3) {
+		printf("usage: cp src dest\n");
+		exit(1);
+	}
+	struct stat st;
+	//check src file
+	if(stat(argv[1], &st)!= 0) {
+		perror(argv[1]);
+		exit(1);
+	}
+	if(S_ISDIR(st.st_mode)) {
+		printf("%s is a directory!\n", argv[1]);
+		exit(1);
+	}
+	//check dest file
+	if(stat(argv[2], &st)==0) {
+		printf("%s already exists!\n", argv[2]);
+		exit(1);
+	}
+	if(S_ISDIR(st.st_mode)) {
+		printf("%s is a directory!\n", argv[2]);
+		exit(1);
+	}
+	
+	//now all files are good
+	int src = open(argv[1], O_RDONLY);
+	if(src < 0) {
+		perror(argv[1]);
+		exit(1);
+	}
+	int dest = open(argv[2], O_WRONLY|O_CREAT);
+	if(dest < 0) {
+		perror(argv[2]);
+		exit(1);
+	}
+	//all files are opened
+	int aantal;
+	char buffer[BUFSIZ];
+	while((aantal=read(src,buffer,BUFSIZ))!=0) {
+		write(dest, buffer,aantal); 
+	}
+	return 0;
+}	
