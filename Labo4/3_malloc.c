ae13ffe69599d8a1660a993d4a03bcf6adcfe692
Lorenzo De Bie <lorenzodebie@ndbsoft.be>
2020-03-11 22:49:34 +0100

    added non-malloc version

--- Labo4/3_malloc.c
+++ Labo4/3_malloc.c
@@ -5,16 +5,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define N 6
+#define MAX 50
 
 int main(int argc, char** argv) {
     //eens proberen met 1 pipe
-    int pids[N];
-    int fds_C_P[N][2];
+    int *pids;
+    int **fds_C_P;
     int i, biggest = 0;
+    if(argc != 2) {
+        printf("Program expects only 1 parameter: number of childs to create!\n");
+        return 1;
+    }
 
-    for(i = 0; i < N; i++) {
+    pids = malloc(sizeof(int)*atoi(argv[1]));
+    fds_C_P = malloc(sizeof(int*) * atoi(argv[1]));
+
+    for(i = 0; i < atoi(argv[1]); i++) {
         //make pipe for communications between child and parent
+        fds_C_P[i] = malloc(sizeof(int) * 2);
         if(pipe(fds_C_P[i]) < 0) {
             perror(argv[0]);
             return 1;
@@ -34,20 +42,30 @@ int main(int argc, char** argv) {
             write(fds_C_P[i][1], &r, sizeof(r));
             close(fds_C_P[i][1]);
             printf("Child %d with pid %d generated: %u\n", i, getpid(), r);
+            //free all the previous allocated arrays for fds
+            for(x = 0; x <= atoi(argv[1]); x++) {
+                free(fds_C_P[x]);
+            }
+            free(fds_C_P);
+            free(pids);
             return 0;
         }
     }
 
     //read from all pipes
-    for(i = 0; i < N; i++) {
+    for(i = 0; i < atoi(argv[1]); i++) {
         int n;
         read(fds_C_P[i][0], &n, sizeof(int));
         if(n > biggest) 
             biggest = n;
+        //free this array of fd
+        free(fds_C_P[i]);
     }
+    free(fds_C_P);
     printf("Biggest number = %d\n", biggest);
-    for(i = 0; i < N; i++) {
+    for(i = 0; i < atoi(argv[1]); i++) {
         waitpid(pids[i], NULL, 0);
     }
+    free(pids);
     return 0;
 }
