090877a0bb278e9d340275561c4f67ec10160833
Lorenzo De Bie <lorenzodebie@ndbsoft.be>
2020-05-19 14:41:50 +0200

    Bash ex 9

--- Bash/Labo/9.sh
+++ Bash/Labo/9.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-while IFS=' ' read user pid ptime rest ; do
-    printf "user: %s pid: %s ptime: %s\n" $user $pid $ptime
-done < <(ps -aux)
+exec {input}< <(ps -aux)
+
+# skip header line
+read header <&${input}
+
+ptimes=( )
+pids=()
+
+while IFS=' ' read user pid ptime rest <&${input} ; do
+    [[ "$$" == $pid ]] && continue
+    #echo $pid takes $ptime% of processor time
+    # loop over the top five processes (can be empty)
+    for((i=0;i<5;i++)); do
+        # if ptimes[i] == empty || ptime bigger than ptimes[i]
+        if [[ -z "${ptimes[i]}" || "$ptime" > "${ptimes[i]}" ]] ; then
+            # insert current pid at index $i
+            ptimes=( "${ptimes[@]:0:i}" "$ptime"  "${ptimes[@]:i}" )
+            pids=( "${pids[@]:0:i}" "$pid"  "${pids[@]:i}" )
+            # we found the spot --> go to next line
+            break
+        fi
+    done
+done
+
+#echo "${ptimes[@]:0:5}"
+echo "${pids[@]:0:5}"
